0a42f7950bd1b49c7d80fb82c5599b0f27348245
Anna <sky.anabet@gmail.com>
2025-09-08 20:12:31 +0300

    five

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-#
-# name = input("Введи своє ім'я: ")
-# age = input("Введи свій вік: ")
-#
-#
-# print(f"Привіт {name}, тобі {age}!")
-#
+my_list = [1, 7, 3]
+print(my_list)
+
+print(my_list[2])
+
+my_list.append(8)
+my_list.append("Hi")
 
 
-age = input("Введи свій вік: ")
-if int(age) > 18:
-    print("Ok")
-else: print("No")
 
 
+for i in 2, 3, 7:
+    print(i)
+
+
+for i in my_list:
+    print(i)
+
+
+word = "Hello"
+for i in word:
+    print(i)
 
